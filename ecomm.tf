d8b83acf9ad2eca0eb6d2c68cdb6963dcf38cf55
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-02-18 19:29:37 -0500

    chaging preview branch
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- ecomm.tf
+++ ecomm.tf
@@ -24,16 +24,16 @@ resource "cloudflare_pages_project" "app" {
   name              = "${local.project_name}"
   production_branch = "gh-pages"
   
+  
   source {
     type = "github"
     config {
       owner                         = "dotcomrow"
       repo_name                     = "ecommerce-store"
       production_branch             = "gh-pages"
-      pr_comments_enabled           = true
-      deployments_enabled           = true
-      production_deployment_enabled = true
-      preview_deployment_setting = "none" 
+      
+      preview_deployment_setting = "custom"
+      preview_branch_includes = ["gh-develop-pages"] 
     }
   }
 
